349ca9da01b74ccf2a28e9ff003e501cd911d98c
Catherine Northcott <cnorthcott@turnpointtech.com>
2017-02-08 21:50:48 -0800

    Testing changes for Google OIDC.

--- social_core/backends/google_openidconnect.py
+++ social_core/backends/google_openidconnect.py
@@ -13,10 +13,10 @@ class GoogleOpenIdConnect(OpenIdConnectAuth):
     # http://openid.net/specs/openid-connect-core-1_0.html#rfc.section.15.6.2
     ID_TOKEN_ISSUER = 'accounts.google.com'
 
-    def user_data(self, access_token, *args, **kwargs):
-        """Return user data from Google API"""
-        return self.get_json(
-            'https://www.googleapis.com/oauth2/v3/userinfo',
-            #'https://www.googleapis.com/plus/v1/people/me/openIdConnect',
-            params={'access_token': access_token, 'alt': 'json'}
-        )
+    # def user_data(self, access_token, *args, **kwargs):
+    #     """Return user data from Google API"""
+    #     return self.get_json(
+    #         'https://www.googleapis.com/oauth2/v3/userinfo',
+    #         #'https://www.googleapis.com/plus/v1/people/me/openIdConnect',
+    #         params={'access_token': access_token, 'alt': 'json'}
+    #     )
